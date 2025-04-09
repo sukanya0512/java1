427e02339d38f2ba4ae29d54983cf069a849e609
sukanya0512 <sukanyamulimani05@gmail.com>
2025-04-09 08:58:24 +0530

    save

--- Addition.java
+++ Addition.java
@@ -1,8 +1,8 @@
-public class Addition{
- public static void main(String[] args){
-      int a=10;
-      int b=5;
-      int sum=a+b;
-      System.out.println("Addition of two number is :"+sum);
- }
+public class Addition {
+      public static void main(String[] args) {
+            int a = 10;
+            int b = 5;
+            int sum = a + b;
+            System.out.println("Addition of two numbers is :" + sum);
+      }
 }
